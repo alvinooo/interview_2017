84ead0eabbca5fa084d86f45166a12e8c19c03f0
alvinheng <a1heng@ucsd.edu>
2017-09-06 21:23:45 -0700

    diameter binary tree

--- data_structures/binary_tree/diameter.py
+++ data_structures/binary_tree/diameter.py
@@ -2,11 +2,11 @@ from BTNode import BTNode
 
 def dfs_diameter(tree):
 	if not tree:
-		return 0
+		return 0, 0
 	left_longest, left_diameter = dfs_diameter(tree.left)
 	right_longest, right_diameter = dfs_diameter(tree.right)
-	return max(left_longest, right_longest) + 1,
-			max(left_longest + 1 + right_longest,
+	return max(left_longest, right_longest) + 1, \
+				max(left_longest + 1 + right_longest, \
 				left_diameter, right_diameter)
 
 root = BTNode(None)
@@ -14,3 +14,32 @@ root.left = BTNode(None)
 root.left.left = BTNode(None)
 root.left.right = BTNode(None)
 root.left.right.right = BTNode(None)
+root.right = BTNode(None)
+root.right.right = BTNode(None)
+root.right.right.right = BTNode(None)
+root.right.right.right.left = BTNode(None)
+root.right.right.right.left.left = BTNode(None)
+root.right.right.right.left.right = BTNode(None)
+root.right.right.right.right = BTNode(None)
+
+_, diameter = dfs_diameter(root)
+print diameter
+
+root = BTNode(None)
+root.left = BTNode(None)
+root.left.left = BTNode(None)
+root.left.left.left = BTNode(None)
+root.left.left.right = BTNode(None)
+root.left.left.right.left = BTNode(None)
+root.left.left.right.left.left = BTNode(None)
+root.left.left.right.left.right = BTNode(None)
+root.left.right = BTNode(None)
+root.left.right.right = BTNode(None)
+root.left.right.right.left = BTNode(None)
+root.left.right.right.right = BTNode(None)
+root.left.right.right.right.right = BTNode(None)
+root.right = BTNode(None)
+root.right.right = BTNode(None)
+
+_, diameter = dfs_diameter(root)
+print diameter
