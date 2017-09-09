5b7eed93209e737302aa2f34976bcba01a97ee40
alvinheng <a1heng@ucsd.edu>
2017-09-08 21:54:11 -0700

    nonadjacent bits

--- algorithms/bit_magic/adjacent.py
+++ algorithms/bit_magic/adjacent.py
@@ -1,3 +1,19 @@
+# 1 2 4 5 8 9 10
+1
+10
+100
+101
+1000
+1001
+1010
+
+import math
+
 def nonadjacent_ones(n):
-	for bit in xrange(n):
-		pass
+	for i in xrange(1, 2 ** n):
+		if i & (i << 1) == 0:
+			print i,
+	print
+
+print nonadjacent_ones(4)
+print nonadjacent_ones(3)
