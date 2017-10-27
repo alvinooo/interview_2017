74198cf66ac11be9bea655ae13c56eb461474c85
alvinheng <a1heng@ucsd.edu>
2017-10-26 23:27:44 -0700

    longest common subsequence

--- algorithms/dynamic/subsequence.py
+++ algorithms/dynamic/subsequence.py
@@ -29,3 +29,48 @@ def test_max_non_adjacent_sum():
 	print max_non_adjacent_sum_constant_space([1, 20, 3])
 
 # test_max_non_adjacent_sum()
+
+def rec_longest_common(s1, s2):
+	if len(s1) == 0 or len(s2) == 0:
+		return []
+	elif s1[-1] == s2[-1]:
+		return rec_longest_common(s1[:-1], s2[:-1]) + [s1[-1]]
+	else:
+		return max(
+			rec_longest_common(s1[:-1], s2),
+			rec_longest_common(s1, s2[:-1]),
+			key=len)
+
+def longest_common(s1, s2):
+	lengths = [[0 for _ in xrange(len(s2) + 1)] for _ in xrange(len(s1) + 1)]
+	for i in xrange(1, len(s1) + 1):
+		for j in xrange(1, len(s2) + 1):
+			if s1[i - 1] == s2[j - 1]:
+				lengths[i][j] = 1 + lengths[i - 1][j - 1]
+			else:
+				lengths[i][j] = max(lengths[i - 1][j], lengths[i][j - 1])
+	subsequence = []
+	i = len(s1)
+	j = len(s2)
+	while i > 0 and j > 0:
+		if s1[i - 1] == s2[j - 1]:
+			subsequence.append(s1[i - 1])
+			i -= 1
+			j -= 1
+		elif lengths[i - 1][j] > lengths[i][j - 1]:
+			i -= 1
+		else:
+			j -= 1
+	# for row in lengths:
+	# 	print row
+	return subsequence[::-1]
+
+def test_rec_longest_common():
+	print rec_longest_common([1,2,3,4,1],[3,4,1,2,1,3])
+	print longest_common([1,2,3,4,1],[3,4,1,2,1,3])
+	print rec_longest_common("ABCDGH","AEDFHR")
+	print longest_common("ABCDGH","AEDFHR")
+	print rec_longest_common("AGGTAB","GXTXAYB")
+	print longest_common("AGGTAB","GXTXAYB")
+
+# test_rec_longest_common()
