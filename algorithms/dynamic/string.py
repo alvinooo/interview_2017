4677a30078fbba353b50105ad5a2ec6bc07d8fa2
alvinheng <a1heng@ucsd.edu>
2017-10-27 21:50:01 -0700

    longest common substring

--- algorithms/dynamic/string.py
+++ algorithms/dynamic/string.py
@@ -13,6 +13,31 @@ def edit_dist(s1, s2):
 					edit_dist_table[i][j - 1], edit_dist_table[i - 1][j])
 	return edit_dist_table[i][j]
 
-print edit_dist("geek", "gesek")
-print edit_dist("cat", "cut")
-print edit_dist("sunday", "saturday")
+def test_edit_dist():
+	print edit_dist("geek", "gesek")
+	print edit_dist("cat", "cut")
+	print edit_dist("sunday", "saturday")
+
+# test_edit_dist()
+
+def longest_common_substring(s1, s2):
+	max_length, max_i, max_j = 0, 0, 0
+	lengths = [[0 for _ in xrange(len(s2) + 1)] for _ in xrange(len(s1) + 1)]
+	for i in xrange(1, len(s1) + 1):
+		for j in xrange(1, len(s2) + 1):
+			if i == 0 or j == 0:
+				lengths[i][j] = 0
+			elif s1[i - 1] == s2[j - 1]:
+				lengths[i][j] = 1 + lengths[i - 1][j - 1]
+				if lengths[i][j] > max_length:
+					max_length = lengths[i][j]
+					max_i, max_j = i, j
+			else:
+				lengths[i][j] = 0
+	return s1[i-max_length:i]
+
+def test_longest_common_substring():
+	print longest_common_substring("caba", "abac")
+	print longest_common_substring("abacdfgdcaba", "abacdgfdcaba")
+
+test_longest_common_substring()
