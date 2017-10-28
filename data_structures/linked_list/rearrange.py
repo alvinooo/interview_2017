29e3a31f23fd106dc24eb2c61f1b8a8c237162d2
alvinheng <a1heng@ucsd.edu>
2017-10-27 18:49:04 -0700

    reverse linked list

--- data_structures/linked_list/rearrange.py
+++ data_structures/linked_list/rearrange.py
@@ -16,10 +16,38 @@ def mid_to_head(head):
 	mid.next = head
 	return mid
 
-l = LLNode(1)
-for i in xrange(2, 6):
-	l.append_value(i)
-print l
-print find_middle(l).value
+def test_mid():
+	l = LLNode(1)
+	for i in xrange(2, 6):
+		l.append_value(i)
+	print l
+	print find_middle(l).value
 
-print mid_to_head(l)
+	print mid_to_head(l)
+
+# test_mid()
+
+def reverse(head):
+	prev = None
+	curr = head
+	while curr:
+		next = curr.next
+		curr.next = prev
+		prev = curr
+		curr = next
+	return prev
+
+def rec_reverse(head):
+	if not head or not head.next:
+		return head
+	else:
+		tail = rec_reverse(head.next)
+		head.next.next = head
+		head.next = None
+		return tail
+
+def test_reverse():
+	print reverse(LLNode(range(1,10)))
+	print rec_reverse(LLNode(range(1,10)))
+
+# test_reverse()
