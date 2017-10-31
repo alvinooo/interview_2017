0564a181602e50a4baf593db12f4995881c7a1df
alvinheng <a1heng@ucsd.edu>
2017-10-30 22:40:07 -0700

    count set bits in range

--- algorithms/bit_magic/set.py
+++ algorithms/bit_magic/set.py
@@ -1,4 +1,19 @@
 def set_even_bits(n):
 	return 0xAA & n
 
-print set_even_bits(30)
+# print set_even_bits(30)
+
+def count_set_bits(n, left, right):
+	n <<= left - 1
+	n >>= right - 1
+	count = 0
+	while n:
+		count += n & 1
+		n >>= 1
+	return count
+
+def test_count_set_bits():
+	print count_set_bits(42, 2, 5)
+	print count_set_bits(79, 1, 4)
+
+# test_count_set_bits()
