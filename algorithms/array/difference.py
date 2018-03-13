655eda82ca06842bb7af5d5548532f579f5dcf71
Alvin Heng <alvin.heng@teradata.com>
2018-03-13 16:02:32 -0700

    i, j, A[i], A[j]

--- algorithms/array/difference.py
+++ algorithms/array/difference.py
@@ -1,3 +1,16 @@
+def max_diff_larger_before_smaller(l):
+	curr_min, max_diff = l[0], l[1] - l[0]
+	for i in xrange(1, len(l)):
+		max_diff = max(max_diff, l[i] - curr_min)
+		curr_min = min(curr_min, l[i])
+	return max_diff
+
+def test_max_diff_larger_before_smaller():
+	print max_diff_larger_before_smaller([2, 3, 10, 6, 4, 8, 1])
+	print max_diff_larger_before_smaller([7, 9, 5, 6, 3, 2])
+
+# test_max_diff_larger_before_smaller()
+
 def buy_sell_stock(prices):
 	local_mins = []
 	local_maxs = []
--- algorithms/array/two_pass.py
+++ algorithms/array/two_pass.py
@@ -14,3 +14,29 @@ def test_product_except_self():
 	print product_except_self([1,2,3,4])
 
 # test_product_except_self()
+
+def max_j_minus_i(l):
+	min_left, max_right = [l[0]], [l[-1]]
+	for i in xrange(1, len(l)):
+		min_left.append(min(min_left[i - 1], l[i]))
+	for i in xrange(len(l) - 2, -1, -1):
+		max_right.insert(0, max(max_right[0], l[i]))
+
+	i, j = 0, 0
+	max_diff = -1
+	while i < len(l) and j < len(l):
+		if min_left[i] < max_right[j]:
+			max_diff = max(max_diff, j - i)
+			j += 1
+		else:
+			i += 1
+
+	return max_diff
+
+def test_max_j_minus_i():
+	print max_j_minus_i([34, 8, 10, 3, 2, 80, 30, 33, 1])
+	print max_j_minus_i([9, 2, 3, 4, 5, 6, 7, 8, 18, 0])
+	print max_j_minus_i([1, 2, 3, 4, 5, 6])
+	print max_j_minus_i([6, 5, 4, 3, 2, 1])
+
+# test_max_j_minus_i()
